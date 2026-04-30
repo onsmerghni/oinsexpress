d107c02d5b552cc43fd3f7703638ee5242061cf4
onsmerghni <merghnichrifa@email.com>
2026-04-30 23:23:49 +0100

    fix: deleteByUserId dans interface

--- src/main/java/com/oinsexpress/repository/PushSubscriptionRepository.java
+++ src/main/java/com/oinsexpress/repository/PushSubscriptionRepository.java
@@ -4,9 +4,9 @@ import com.oinsexpress.entity.PushSubscriptionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.UUID;
-void deleteByUserId(UUID userId);
 
 public interface PushSubscriptionRepository extends JpaRepository<PushSubscriptionEntity, UUID> {
     List<PushSubscriptionEntity> findByUserId(UUID userId);
     void deleteByEndpoint(String endpoint);
+    void deleteByUserId(UUID userId);
 }
